c69df3fc2d457507c6fe00659688b40fbb6a0d22
Runjun Mao <122883183+MartinMao2023@users.noreply.github.com>
2023-01-31 22:49:00 +0000

    Updated

--- covariance.py
+++ covariance.py
@@ -1,19 +1,33 @@
 import numpy as np
+import re
 
+input_pattern = re.compile(r"^[+-]?\d+\.?\d*,[+-]?\d+\.?\d*$")
 
 def covariance():
 
-    coor_list = []
-    for i in range(10):
+    coor_array = np.zeros(10, 2, dtype=np.float64)
+    valid_input = 0
+    dictionary = {0: 'st', 1:"nd", 2:"rd", 3:"th"}
+    
+    while valid_input < 10:
+        while True:
+            
+            coor = input(f"Please enter the {valid_input+1}{dictionary[valid_input if valid_input < 3 else 3]} coordinate in the form x, y:").replace(" ", "")
+            if input_pattern.fullmatch(coor):
+                x, y = coor.split(",")
+                coor_array[valid_input, 0] = float(x)
+                coor_array[valid_input, 1] = float(y)
+                break
+            else:
+                print("Invalid input, please try again")
+        valid_input += 1
 
-        coor = input(f"Please enter {i+1} coordinate in the form x, y:")
-        coor_list.append([float(coor.split(",")[0]), float(coor.split(",")[-1])])
+    print(f"The 10 coordinates you entered are: {coor_array}")
 
-    print(f"The 10 coordinates you entered are: {coor_list}")
+    cov = np.cov(coor_array.T, ddof=0)
+    mean = np.mean(coor_array, axis=0)
 
-    coor_array = np.array(coor_list).T
-    cov = np.cov(coor_array, ddof=0)
-
+    print(f"The mean vector is {mean}")
     print(f"Covariance Matrix: {cov}")
 
     return cov
