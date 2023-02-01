6f4cea1a24c881d3fcd21be72b8a0383259d1a2b
Runjun Mao <122883183+MartinMao2023@users.noreply.github.com>
2023-02-01 09:33:06 +0000

    Update covariance.py

--- covariance.py
+++ covariance.py
@@ -1,25 +1,36 @@
 import numpy as np
 import re
 
-input_pattern = re.compile(r"^[+-]?\d+\.?\d*,[+-]?\d+\.?\d*$")
+
 
 def covariance():
 
-    coor_array = np.zeros(10, 2, dtype=np.float64)
+    coor_array = np.zeros((10, 2), dtype=np.float32)
     valid_input = 0
     dictionary = {0: 'st', 1:"nd", 2:"rd", 3:"th"}
-    
+#     input_pattern = re.compile(r"^[+-]?\d+\.?\d*,[+-]?\d+\.?\d*$")
     while valid_input < 10:
         while True:
             
             coor = input(f"Please enter the {valid_input+1}{dictionary[valid_input if valid_input < 3 else 3]} coordinate in the form x, y:").replace(" ", "")
-            if input_pattern.fullmatch(coor):
+#             if input_pattern.fullmatch(coor):
+#                 x, y = coor.split(",")
+#                 coor_array[valid_input, 0] = float(x)
+#                 coor_array[valid_input, 1] = float(y)
+#                 break
+#             else:
+#                 print("Invalid input, please try again")
+                
+            try:
                 x, y = coor.split(",")
-                coor_array[valid_input, 0] = float(x)
-                coor_array[valid_input, 1] = float(y)
-                break
-            else:
+                x = float(x)
+                y = float(y)
+            except Exception:
                 print("Invalid input, please try again")
+            else:
+                coor_array[valid_input, 0] = x
+                coor_array[valid_input, 1] = y
+                break
         valid_input += 1
 
     print(f"The 10 coordinates you entered are: {coor_array}")
