e95df59e2e821efd3edb38fb2e8a0dbf4d436fe8
RunjunMao <rm522@ic.ac.uk>
2023-02-07 11:23:27 +0000

    Version 1.2

--- RobotMotion.py
+++ RobotMotion.py
@@ -1,37 +1,32 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-
 import time
 from datetime import datetime
-import brickpi3
-
-
-BP = brickpi3.BrickPi3()
-
-degree_to_distance = 0.0486 
-wheel_separation = 13.85 
 
 
 class Robot:
-    def __init__(self, bp, left_motor="A", right_motor="D", degree_to_distance=0.0486, wheel_separation=13.85, power_limit=70, dps_limit=400, sonar=0):
+    def __init__(self, bp, left_motor="A", right_motor="D", 
+        degree_to_distance=0.0486, wheel_separation=13.85, 
+        power_limit=70, dps_limit=400, 
+        sonar=0):
         """
-        Left motor is always PORT_A, right motor is always PORT_D
+        The Robot class for brickpi3 robot.
         """
         self.bp = bp
         self.D = degree_to_distance
         self.W = wheel_separation
-
-        self.dps_limit = dps_limit
-        self.speed_limit = dps_limit * self.D
-        self.power_limit = power_limit
-
+        # Set up limits
+        self.dps_limit = dps_limit - 10
+        self.speed_limit = self.dps_limit * self.D
+        self.power_limit = power_limit-5
+        # Ports connecting to motors and sensors
         self.motors = {"A": bp.PORT_A, "B": bp.PORT_B, "C": bp.PORT_C, "D": bp.PORT_D}
         self.sensors = {1: bp.PORT_1, 2: bp.PORT_2, 3: bp.PORT_3, 4: bp.PORT_4}
-
+        # Set motor ports
         self.left_motor = self.motors[left_motor]
         self.right_motor = self.motors[right_motor]
-
+        # Set sensor ports
         self.sonar = self.sensors[sonar] if sonar else 0
         try:
             # Reset encoders
@@ -43,39 +38,41 @@ class Robot:
         except IOError:
             raise
 
-    
+
     @property
     def status(self):
         left_status = self.bp.get_motor_status(self.left_motor)
         right_status = self.bp.get_motor_status(self.right_motor)
         return left_status, right_status
 
+
     @property
     def speed(self):
         left_speed = self.bp.get_motor_status(self.left_motor)[-1]*self.D
         right_speed = self.bp.get_motor_status(self.right_motor)[-1]*self.D
         return left_speed, right_speed
-    
+
+
     @speed.setter
     def speed(self, *speeds):
         if len(speeds) == 1:
             if abs(speeds) > self.speed_limit:
-                right_wheel_speed = left_wheel_speed = self.dps_limit - 10 if speeds > 0 else 10 - self.dps_limit
+                right_wheel_speed = left_wheel_speed = self.dps_limit if speeds > 0 else -self.dps_limit
             else:
                 right_wheel_speed = left_wheel_speed = speeds / self.D
         else:
             left_speed, right_speed = speeds
             if abs(left_speed) > self.speed_limit:
-                left_wheel_speed = self.dps_limit - 10 if left_speed > 0 else 10 - self.dps_limit
+                left_wheel_speed = self.dps_limit if left_speed > 0 else -self.dps_limit
             else:
                 left_wheel_speed = left_speed / self.D
             if abs(right_speed) > self.speed_limit:
-                right_wheel_speed = self.dps_limit - 10 if right_speed > 0 else 10 - self.dps_limit
+                right_wheel_speed = self.dps_limit if right_speed > 0 else self.dps_limit
             else:
                 right_wheel_speed = right_speed / self.D
-            
-            self.bp.set_motor_dps(self.left_motor, left_wheel_speed)
-            self.bp.set_motor_dps(self.right_motor, right_wheel_speed)
+        # Set motor angular speed
+        self.bp.set_motor_dps(self.left_motor, left_wheel_speed)
+        self.bp.set_motor_dps(self.right_motor, right_wheel_speed)
 
 
     @property
@@ -84,7 +81,7 @@ class Robot:
         right_encoder = self.bp.get_motor_encoder(self.right_motor)
         return left_encoder, right_encoder
 
-    
+
     def clear_encoder(self):
         self.bp.offset_motor_encoder(self.left_motor, self.bp.get_motor_encoder(self.left_motor))
         self.bp.offset_motor_encoder(self.right_motor, self.bp.get_motor_encoder(self.right_motor))
@@ -93,43 +90,80 @@ class Robot:
     def stop(self):
         self.bp.set_motor_dps(self.left_motor, 0)
         self.bp.set_motor_dps(self.right_motor, 0)
-        return None
-    
+        time.sleep(0.1)
+
 
     def move(self, distance, speed=None, start_delay=0, finish_delay=0):
         if start_delay > 0:
             time.sleep(start_delay)
+        # Reset encoders to 0
         self.clear_encoder()
-
+        # Make the robot move forward
         if speed:
             if abs(speed) > self.speed_limit:
-                speed = self.speed_limit -10 if speed > 0 else 10 - self.speed_limit
+                speed = self.speed_limit if speed > 0 else -self.speed_limit
             estimated_time = distance / speed
             self.speed = speed
-            time.sleep(estimated_time-0.2)
-            current_speed = sum(self.speed)/2
-            current_position = sum(self.encoder)/2 * self.D
-            remaining_time = 0.2 + (distance - current_position) / current_speed
+            time.sleep(estimated_time - 0.2)
+            # Correction
+            remaining_time = 0.2 + (2*distance - sum(self.encoder)*self.D) / sum(self.speed)
             time.sleep(remaining_time)
             self.stop()
-        else:
+        else: # Use position control if speed is not specified
             angular_target = distance / self.D
             self.bp.set_motor_position(self.left_motor, angular_target)
             self.bp.set_motor_position(self.right_motor, angular_target)
             while True:
+                # Check every 50 ms
                 time.sleep(0.05)
                 left_encoder, right_encoder = self.encoder
                 if max(abs(left_encoder - angular_target), abs(right_encoder - angular_target)) < 20:
+                    self.stop()
                     break
-
+        # Wait if required
         if finish_delay > 0:
             time.sleep(finish_delay)
-            
-        return self.encoder*self.D
+        # Return the actual traveled distance (in cm)
+        return sum(self.encoder)*self.D/2
 
 
-    def rotate(self, angle, angular_speed, start_delay, finish_delay):
-        pass
+    def rotate(self, angle, angular_speed=None, start_delay=0, finish_delay=0):
+        if start_delay > 0:
+            time.sleep(start_delay)
+        # Calculate the angular target (could be negative)
+        angular_target = angle * self.W / (self.D * 57.296)
+        # Reset encoder to 0
+        self.clear_encoder()
+        # Rotate the robot
+        if angular_speed:
+            speed = angular_speed * self.W / 114.59
+            if abs(speed) > self.speed_limit:
+                speed = self.speed_limit if speed > 0 else -self.speed_limit
+                estimated_time = angular_target * self.D / (2*speed)
+                self.speed = -speed, speed
+                time.sleep(estimated_time - 0.2)
+                # Correction
+                left_speed, right_speed = self.speed
+                left_encoder, right_encoder = self.encoder
+                remaining_time = (angular_target - right_encoder + left_encoder) * self.D / (right_speed - left_speed) + 0.2
+                time.sleep(remaining_time)
+                self.stop()
+        else: # Use position control if angular speed is not specified
+            self.bp.set_motor_position(self.left_motor, angular_target/2)
+            self.bp.set_motor_position(self.right_motor, -angular_target/2)
+            while True:
+                # Check every 50 ms
+                time.sleep(0.05)
+                left_encoder, right_encoder = self.encoder
+                if abs(right_encoder - left_encoder - angular_target) < 20:
+                    self.stop()
+                    break
+        # Wait if required
+        if finish_delay > 0:
+            time.sleep(finish_delay)
+        # Return the actual rotated angle (in degree)
+        left_encoder, right_encoder = self.encoder
+        return (right_encoder - left_encoder) * self.D / self.W * 57.296
 
 
     def circle(self):
@@ -149,122 +183,18 @@ class Robot:
 
 
 
+if __name__ == "__main__":
+    import brickpi3
 
-def move_forward(distance, speed, starting_delay=0, finish_delay=0):
-    """ Make the robot move forward.
-    Unit for degree is "degree".
-    Unit for distance is "cm".
-    Unit for time is "second".
-
-    Args:
-        distance (float): Distance to move by the robot.
-        speed (float): Speed of the robot.
-        starting_delay (float): Extra waiting time before excution.
-        finish_delay (float): Waiting time after completion.
-    """
-    global BP
-    starting_delay = starting_delay if starting_delay > 0 else 0
-    speed = 399*degree_to_distance if speed/degree_to_distance > 399 else speed
-    # Calculate the desired wheel speed and the estimated time to execute the action
-    wheel_angular_speed = speed/degree_to_distance
-    time_estimate = distance / speed
-
-    # Stop the motors at first
-    BP.set_motor_dps(BP.PORT_A, 0)
-    BP.set_motor_dps(BP.PORT_D, 0)
-    if abs(BP.get_motor_status(BP.PORT_A)[-1]) > 1 or abs(BP.get_motor_status(BP.PORT_D)[-1]) > 1:
-        time.sleep(0.5 + starting_delay)
-    elif starting_delay > 0:
-        time.sleep(starting_delay)
-
-    # Set the motor speed, wait for execution and then stop the motors.
-    BP.set_motor_dps(BP.PORT_A, wheel_angular_speed)
-    BP.set_motor_dps(BP.PORT_D, wheel_angular_speed)
-    time.sleep(time_estimate)
-    BP.set_motor_dps(BP.PORT_A, 0)
-    BP.set_motor_dps(BP.PORT_D, 0)
-
-    if finish_delay > 0:
-        time.sleep(finish_delay)
-
-
-def rotate(angle, angular_speed, clockwise=0, starting_delay=0, finish_delay=0):
-    """ Make the robot move rotate.
-    Unit for degree is "degree".
-    Unit for distance is "cm".
-    Unit for time is "second".
-
-    Args:
-        angle (float): Angle to rotate by the robot.
-        angular speed (float): angular speed of the robot.
-        clockwise (Boolean): Whether to rotate clockwise.
-        starting_delay (float): Extra waiting time before excution.
-        finish_delay (float): Waiting time after completion.
-    """
-
-    global BP 
-    starting_delay = starting_delay if starting_delay > 0 else 0
-    wheel_angular_speed = wheel_separation * angular_speed / (114.6 * degree_to_distance)
-    if wheel_angular_speed > 399:
-        wheel_angular_speed = 399
-        angular_speed = (45722 * degree_to_distance) / wheel_separation
-    # Calculate the desired wheel speed and the estimated time to execute the action
-    time_estimate = angle / angular_speed
-    wheel_angular_speed = wheel_separation * angular_speed / (114.6 * degree_to_distance)
-
-    # Assign the motor speed based on direction of rotation
-    if clockwise:
-        left_wheel_angular_speed = wheel_angular_speed
-        right_wheel_angular_speed = -wheel_angular_speed
-    else:
-        left_wheel_angular_speed = -wheel_angular_speed
-        right_wheel_angular_speed = wheel_angular_speed
-
-    # Stop the motors at first
-    BP.set_motor_dps(BP.PORT_A, 0)
-    BP.set_motor_dps(BP.PORT_D, 0)
-    if abs(BP.get_motor_status(BP.PORT_A)[-1]) > 1 or abs(BP.get_motor_status(BP.PORT_D)[-1]) > 1:
-        time.sleep(0.5 + starting_delay)
-    elif starting_delay > 0:
-        time.sleep(starting_delay)
-
-    # Set the motor speed, wait for execution and then stop the motors.
-    BP.set_motor_dps(BP.PORT_A, left_wheel_angular_speed)
-    BP.set_motor_dps(BP.PORT_D, right_wheel_angular_speed)
-    time.sleep(time_estimate)
-    BP.set_motor_dps(BP.PORT_A, 0)
-    BP.set_motor_dps(BP.PORT_D, 0)
-
-    if finish_delay > 0:
-        time.sleep(finish_delay)
-
-
-
-try:
-    try: # Initializing 
-        BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A)) # reset encoder A
-        BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder D
-        # Set up motor limits
-        BP.set_motor_limits(BP.PORT_A, 70, 400) 
-        BP.set_motor_limits(BP.PORT_D, 70, 400)
-    except IOError as error:
-        print(error)
-        raise
-
-    # Draw a 40cm x 40 cm square
-    move_forward(distance=40, speed=8, starting_delay=2, finish_delay=1)
-    rotate(angle=90, angular_speed=30, starting_delay=1, finish_delay=1)
-    move_forward(distance=40, speed=8, starting_delay=1, finish_delay=1)
-    rotate(angle=90, angular_speed=30, starting_delay=1, finish_delay=1)
-    move_forward(distance=40, speed=8, starting_delay=1, finish_delay=1)
-    rotate(angle=90, angular_speed=30, starting_delay=1, finish_delay=1)
-    move_forward(distance=40, speed=8, starting_delay=1, finish_delay=1)
-    rotate(angle=90, angular_speed=30, starting_delay=1, finish_delay=1)
-    
-    # Loose the motors
-    BP.set_motor_power(BP.PORT_A, BP.MOTOR_FLOAT)
-    BP.set_motor_power(BP.PORT_D, BP.MOTOR_FLOAT)
-except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
-    BP.reset_all()
-
-
+    # Example
+    BP = brickpi3.BrickPi3()
+    robot = Robot(BP)
+    try:
+        distance = robot.move(30, 6, finish_delay=0.5)
+        print("distance traveled:", distance)
+        angle = robot.rotate(180, 30, finish_delay=0.5)
+        print("angle turned:", angle)
+        distance = robot.move(30, 6)
+        print("distance traveled:", distance)
+    except:
+        robot.shutdown()
