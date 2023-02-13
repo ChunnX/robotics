5692424d383e863d0ea3c26c764365afc0522111
Runjun Mao <122883183+MartinMao2023@users.noreply.github.com>
2023-02-13 15:22:02 +0000

    Update and rename turn angle to turn angle.py

---
+++ turn angle.py
@@ -0,0 +1,51 @@
+def navigateToWaypoint(X, Y):
+    global robot_position
+    # Y - robot_position[1] -> robot_position[1] - Y
+    dx, dy = X - robot_position[0], robot_position[1] - Y
+    distance = math.sqrt(dx**2 + dy**2)
+    alpha = -math.atan2(dy, dx)
+    beta = alpha - robot_position[2]
+    rot(beta, 3)
+    go(distance/scale, 3)
+
+
+
+if __name__ == "__main__":
+    import brickpi3
+    from RobotMotion import Robot
+    # Example
+    BP = brickpi3.BrickPi3()
+    robot = Robot(BP)
+    
+    position = np.zeros(2, dtype=np.float32)
+    angle = 0
+    
+    try:
+        while True:
+            while True:
+                coordinates = input("Coordinates ")
+                try:
+                    x, y = coordinates.split(",")
+                    x = float(x)
+                    y = float(y)
+                except:
+                    print("Invalid input")
+                else:
+                    break
+            dx = x - position[0]
+            dy = y - position[1]
+            
+            angle_to_turn = np.arctan2(dx, dy) * 57.296 - angle
+            
+            turned_angle = robot.rotate(angle_to_turn, 30, finish_delay=0.5)
+            moved_distance = robot.move(np.sqrt(dx**2+dy**2), 8, finish_delay=0.5)
+            angle += turned_angle
+            dx = np.cos(angle / 57.296) * moved_distance
+            dy = np.sin(angle / 57.296) * moved_distance
+            position[0] += dx
+            position[1] += dy
+            print(position, angle)
+        pass
+
+    except:
+        robot.shutdown()
