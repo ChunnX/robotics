bde850ef86de9144a40b044ec5651f365a956695
RunjunMao <rm522@imperial.ac.uk>
2023-02-13 15:33:39 +0000

    simulate

--- simulator/RobotMotion.py
+++ simulator/RobotMotion.py
@@ -8,14 +8,14 @@ class Robot:
         self.W = 14.36
         self.encoder = np.zeros(2, dtype=np.int32)
     
-    def move(self, distance, speed=5, finish_delay=0):
+    def move(self, distance, speed=5, start_delay=0 finish_delay=0):
         self.clear_encoder()
         degree = distance / self.D
         self.encoder[0] += np.int32(degree + int(np.random.normal(0, 5)))
         self.encoder[1] += np.int32(degree + int(np.random.normal(0, 5)))
         return np.mean(self.encoder) * self.D
     
-    def rotate(self, angle, angular_speed=30, finish_delay=0):
+    def rotate(self, angle, angular_speed=30, start_delay=0, finish_delay=0):
         self.clear_encoder()
         degree = angle * self.W / (114.591*self.D)
         self.encoder[0] += np.int32(-degree + int(np.random.normal(0, 5)))
