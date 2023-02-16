88b3eda29803d30c09cd91f2b8f3b3a0e3e2cfcc
RunjunMao <rm522@ic.ac.uk>
2023-02-16 21:12:28 +0000

    update calibration

---
+++ new_calibrator.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+def read_input():
+    while True:
+        try:
+            e1 = float(input("e1: "))
+        except Exception:
+            print("Invalid input")
+        else:
+            break
+    
+    while True:
+        try:
+            e2 = float(input("e2: "))
+        except Exception:
+            print("Invalid input")
+        else:
+            break
+    
+    while True:
+        try:
+            delta_e = float(input("delta e: "))
+        except Exception:
+            print("Invalid input")
+        else:
+            break
+
+    while True:
+        try:
+            l = float(input("l: "))
+        except Exception:
+            print("Invalid input")
+        else:
+            break
+    
+    return e1, e2, delta_e, l
+
+
+
+if __name__ == "__main__":
+    e1 = []
+    e2 = []
+    delta_e = []
+    target = []
+
+    while True:
+        try:
+            e_1, e_2, delta_e_, l = read_input()
+            e1.append(e_1)
+            e2.append(e_2)
+            delta_e.append(delta_e_)
+            target.append(l)
+        except KeyboardInterrupt:
+            e1 = np.array(e1)
+            e2 = np.array(e2)
+            delta_e = np.array(delta_e)
+            target = np.array(target)
+            break
+
+    # e1 = np.array([512,512])
+    # e2 = np.array([512,512])
+    # delta_e = np.array([791,893])
+    # target = np.array([-12.3, -4.1])
+    
+    lr = 0.00000015
+    epochs = 5000
+    r = 0.03333
+    D = 0.05
+    for epoch in range(epochs):
+        theta = r*delta_e/10
+        theta = theta - 2*np.pi * np.floor(theta/(np.pi*2))
+        predictions = np.sqrt(e1**2 + e2**2 + 2*e1*e2*np.cos(theta)) * D
+        predictions = np.where(theta > np.pi, predictions, -predictions)
+
+        delta_l = 2 / len(target) * (predictions - target)
+        delta_l = delta_l.reshape(1, -1)
+
+        dl_dD = predictions / D
+        dl_dr = e1 * e2 * np.abs(np.sin(theta)) * delta_e / np.sqrt(e1**2 + e2**2 + 2*e1*e2*np.cos(theta)) * D / 10
+        dl_dD = dl_dD.reshape(-1, 1)
+        dl_dr = dl_dr.reshape(-1, 1)
+
+        dL_dD = float(delta_l @ dl_dD)
+        dL_dr = float(delta_l @ dl_dr)
+
+        r -= dL_dr * lr
+        D -= dL_dD * lr
+
+        if (epoch + 1) % 100 == 0:
+            print("loss:", np.sqrt(sum((target-predictions)**2)))
+            if dL_dr * lr < 1e-8 and dL_dD * lr < 1e-10:
+                print("early stop")
+                break
+
+    print("D:", round(D, 8))
+    print("W:", round(D / r * 10, 4))
+
+
+
--- robot_calibration.py
+++ robot_calibration.py
@@ -10,15 +10,21 @@ BP = brickpi3.BrickPi3()
 robot = Robot(BP)
 
 angle = 170
+l1 = 40
+l2 = 40
 try:
-    robot.move(40, 10, finish_delay=1)
-    d1 = sum(robot.encoder) / 2
-    theta = robot.rotate(angle, finish_delay=1)
-    robot.move(40, 10)
+    robot.move(l1, 10, finish_delay=1)
+    left_encoder, right_encoder = robot.encoder
+    d1 = (left_encoder + right_encoder) / 2
+    deviation = right_encoder - left_encoder
+    robot.rotate(angle, finish_delay=1)
+    left_encoder, right_encoder = robot.encoder
+    delta_e =  right_encoder - left_encoder + deviation
+    robot.move(l2, 10)
     d2 = sum(robot.encoder) / 2
     print("d1:", round(d1, 1))
     print("d2:", round(d2, 1))
-    print("theta:", round(theta, 2))
+    print("delta_e:", delta_e)
     print("D:", robot.D, "W:", robot.W)
 except:
     robot.shutdown()
