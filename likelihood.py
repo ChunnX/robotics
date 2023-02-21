ae3867e490c5ff6e553e05da2e40df622b5bfda7
RunjunMao <rm522@ic.ac.uk>
2023-02-21 22:34:26 +0000

    Faster version

--- likelihood.py
+++ likelihood.py
@@ -15,8 +15,8 @@ def calculate_likelihood(x, y, theta, z):
     garbage_rate = 0.002
 
     # collections of the walls
-    walls = np.array(
-        [[[0, 0], [0, 168]],  # a
+    walls = np.array([
+        [[0, 0], [0, 168]],  # a
         [[0, 168], [84, 168]],  # b
         [[84, 126], [84, 210]],  # c
         [[84, 210], [168, 210]],  # d
@@ -24,34 +24,45 @@ def calculate_likelihood(x, y, theta, z):
         [[168, 84], [210, 84]],  # f
         [[210, 84], [210, 0]],  # g
         [[210, 0], [0, 0]]  # h
-    ], dtype=np.float32)
+        ], dtype=np.float32)
 
     position = np.array([x, y], dtype=np.float32)
-    v = np.array([np.cos(0.0174533 * theta), np.sin(0.0174533 * theta)], dtype=np.float32)
     distance = np.inf
     wall_index = 0
     
+    vx = np.cos(0.0174533 * theta)
+    vy = np.sin(0.0174533 * theta)
+    
     for index, (pa, pb) in enumerate(walls):
-        a = pa - position
-        b = pb - position
-        det = np.cross(v, b-a)
-        d = float(np.cross(a, b) / det)
+        ax, ay = pa - position
+        bx, by = pb - position
+        det = vx*(by - ay) - vy*(bx - ax)
+        d = (ax*by - ay*bx) / det
         if d > 0:
-            ratio = float(np.cross(v, b) / det)
+            ratio = (vx*by - vy*bx) / det
             if 0 < ratio < 1:
                 if d < distance:
                     distance = d
                     wall_index = index
 
     pa, pb = walls[wall_index]
-    print(f"The robot is facing wall {walls[wall_index]}, with expected depth measurement {distance}")
+    dx, dy = pb - pa
+    # print(f"The robot is facing wall {walls[wall_index]}, with expected depth measurement {distance}")
 
-    cosine_angle = abs(float(v.T @ (pb - pa)))
+    cosine_angle = abs(vx*dy + vy*dx)
     if cosine_angle < 0.7071:
         return garbage_rate
     else:
         likelihood = np.exp(-(z - distance)**2 / (2*sonar_sigma**2)) + garbage_rate
         return likelihood
 
+# import datetime
 
+# start = datetime.datetime.now()
+# for i in range(100):
+#     z = calculate_likelihood(50 + 1.5*i, 30, 110, 30)
+
+# end = datetime.datetime.now()
+# time_cost = (end - start).total_seconds()
+# print("cost:", round(time_cost, 4), "seconds")
 
