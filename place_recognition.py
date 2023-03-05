e8eb5c80dfff0f05ca864fd02c8bb9fcdae2e4bf
RunjunMao <rm522@ic.ac.uk>
2023-03-05 18:37:52 +0000

    updated

--- place_recognition.py
+++ place_recognition.py
@@ -16,65 +16,20 @@ if __name__ == "__main__":
         current_angle = 0
 
         # 0 to 330 degree, 30 degree as interval
-        for i in range(12):
-            if i:
-                angle_to_turn = i*30 - current_angle
-                rotated_angle = robot.rotate(angle_to_turn, 60, finish_delay=0.2)
-                current_angle += rotated_angle
-            
-            sonar_readings = [0, 0, 0, 0, 0]
+        for i in range(36):
+            target_angle = i*10
+            angle_to_turn = target_angle - current_angle
+            if angle_to_turn:
+                rotated_angle = robot.rotate(angle_to_turn, 45, finish_delay=0.1)
+            current_angle += rotated_angle
 
+            sonar_readings = [0, 0, 0, 0, 0]
             for j in range(5):
                 sonar_readings[j] = robot.sonar 
                 time.sleep(0.01)
             
             sonar_reading = median(sonar_readings)
-            detection_dictionary[30*i] = sonar_reading
-        
-        angle_to_turn = 370 - current_angle 
-        current_angle += robot.rotate(angle_to_turn, 60, finish_delay=0.5)
-        current_angle -= 360
-
-        # 10 to 340, 30 degree as interval
-        for i in range(12):
-            if i:
-                angle_to_turn = 10 + i*30 - current_angle
-                rotated_angle = robot.rotate(angle_to_turn, 60, finish_delay=0.2)
-                current_angle += rotated_angle
-            
-            sonar_readings = [0, 0, 0, 0, 0]
-
-            for j in range(5):
-                sonar_readings[j] = robot.sonar 
-                time.sleep(0.01)
-                        
-            sonar_reading = median(sonar_readings)
-            detection_dictionary[30*i + 10] = sonar_reading
-        
-        angle_to_turn = 380 - current_angle 
-        current_angle += robot.rotate(angle_to_turn, 60, finish_delay=0.5)
-        current_angle -= 360
-
-        # 20 to 350, 30 degree as interval
-        for i in range(12):
-            if i:
-                angle_to_turn = 20 + i*30 - current_angle
-                rotated_angle = robot.rotate(angle_to_turn, 60, finish_delay=0.2)
-                current_angle += rotated_angle
-            
-            sonar_readings = [0, 0, 0, 0, 0]
-
-            for j in range(5):
-                sonar_readings[j] = robot.sonar 
-                time.sleep(0.01)
-                        
-            sonar_reading = median(sonar_readings)
-            detection_dictionary[20 + 30*i] = sonar_reading
-
-
-        angle_back_to_start = 360 - current_angle
-        robot.rotate(angle_back_to_start, 30)
-
+            detection_dictionary[target_angle] = sonar_reading
     
     except KeyboardInterrupt:
         robot.shutdown()
