b8005e4cf3d765cc5a22f4fce119048c7c2f6b97
RunjunMao <rm522@ic.ac.uk>
2023-03-06 22:51:15 +0000

    updated

--- robot_calibration.py
+++ robot_calibration.py
@@ -9,21 +9,27 @@ import time
 BP = brickpi3.BrickPi3()
 robot = Robot(BP)
 
-angle = 170
+angle = 180
 l1 = 40
 l2 = 40
 try:
-    robot.move(l1, 10, finish_delay=1)
+    robot.move(l1, 24, finish_delay=1)
     left_encoder, right_encoder = robot.encoder
     el_1, er_1 = left_encoder, right_encoder
-    robot.rotate(angle, finish_delay=1)
+    robot.rotate(angle, 90, finish_delay=1)
     elt, ert = robot.encoder
-    robot.move(l2, 10)
+    print("delta_e:", ert*robot.r - elt)
+    robot.move(l2, 24)
     el_2, er_2 = robot.encoder
     print("e1l, e1r:", el_1, er_1)
     print("etl, etr", elt, ert)
     print("e2l, e2r:", el_2, er_2)
+except KeyboardInterrupt:
+    robot.shutdown()
 except:
+    robot.shutdown()
+    raise
+else:
     robot.shutdown()
 
 
